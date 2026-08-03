fd599a4feaaf0a866364bd0213f79426e19a235c
randomNinja64 <43556413+randomNinja64@users.noreply.github.com>
2026-08-03 11:12:21 -0500

    Update Version Number

--- NyoCoder-VSIX-2017/Properties/AssemblyInfo.cs
+++ NyoCoder-VSIX-2017/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("1.4.0.0")]
-[assembly: AssemblyFileVersion("1.4.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
